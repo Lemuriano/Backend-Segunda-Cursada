1163d80617c31773986cff87525fa3138f8a58dc
Lemuriano <bernabegoitia@gmail.com>
2023-09-05 21:39:45 -0300

    Agregado login, sesiones, cookies, bloqueo sin autentificacion

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ const db = 'ProyectoCoder'
 
 // Servers para pruebas online/host
 
-const server = `mongodb+srv://bernabegoitia:0pU9UELbPziCZxUO@cluster0.dkag3v1.mongodb.net/${db}?retryWrites=true&w=majority`
+export const server = `mongodb+srv://bernabegoitia:0pU9UELbPziCZxUO@cluster0.dkag3v1.mongodb.net/${db}?retryWrites=true&w=majority`
 // export const server = `mongodb://localhost:27017/${db}`
 
 
